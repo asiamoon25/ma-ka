971c37df38ca71ad606d4f167caf9ae57856df79
첫 커밋 <>
2022-08-16 22:00:49 +0900

    2022-08-16 UserInfo params change

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/userinfo',function(req,res,next){
-  const userName = req.body.action.params.name;
+  const userName = req.body.userRequest.utterance.split(' ').replace(/\n/g,"");
   console.log(req.body)
   crawling.userInfo(userName).then(r=>{
     res.json({
