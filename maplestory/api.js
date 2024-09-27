53f35175b8f8bfdbbcb3134a0889a16781da3719
yeonsang <wjddustkd45@gmail.com>
2024-09-27 15:24:28 +0900

    feat(character) : maple api character 부분 개발 중
    * 기본 정보 부터 장비 조회 까지 개발 완

--- maplestory/api.js
+++ maplestory/api.js
@@ -5,6 +5,7 @@ const {isValidDate} = require("../util/util");
 require('dotenv').config();
 const MAPLE_API_KEY = process.env.MAPLE_API_KEY;
 const MAPLE_API_URL = process.env.MAPLE_API_URL;
+
 exports.getCharacterOCID = async function getCharacterOCID(characterName) {
     try{
         var ocid;
@@ -23,38 +24,209 @@ exports.getCharacterOCID = async function getCharacterOCID(characterName) {
     }
 }
 
-exports.getCharacterBasic = async function getCharacterBasic(ocid, date) {
+exports.getCharacterBasicInfo = async function getCharacterBasic(ocid) {
     try{
         if(!ocid || typeof ocid !== 'string') {
             throw new Error('Invalid OCID');
         }
-        if(!isValidDate(date)) {
-            throw new Error('Invalid date format. Expected YYYY-MM-DD')
-        }
-        console.log(date);
 
         const response = await axios({
-            url : MAPLE_API_URL + '/maplestory/v1/character/basic',
+            url : MAPLE_API_URL + `/maplestory/v1/character/basic?ocid=${encodeURIComponent(ocid)}`,
             method: "GET",
             headers: {
                 'x-nxopen-api-key' : MAPLE_API_KEY,
                 'content-type': 'application/json'
-            },
-            params: {
-                ocid: ocid,
-                date: date
             }
         });
 
-        console.log(response);
+        // console.log('response : ' + JSON.stringify(response.data, null, 2));
 
-        return response;
+        return response.data;
     }catch(error) {
         if(error.response) {
             console.error('API Error Reponse : ' , error.response.data);
         }else{
             console.error('Request Error : ', error.message)
         }
+        throw error;
+    }
+}
+
+exports.getCharacterPopularityInfo = async function getCharacterPopularity(ocid) {
+    try{
+        if(!ocid || typeof ocid !== 'string') {
+            throw new Error('Invalid OCID');
+        }
+
+        const response = await axios({
+            url : MAPLE_API_URL + '/maplestory/v1/character/popularity',
+            method: 'GET',
+            headers: {'x-nxopen-api-key' : MAPLE_API_KEY},
+            params: {
+                ocid : encodeURIComponent(ocid)
+            }
+        });
+
+        console.log(response.data);
+
+        return response.data;
+    }catch(error) {
+        if(error.response) {
+            console.error('API Error Reponse : ' , error.response.data);
+        }else{
+            console.error('Request Error : ', error.message)
+        }
+        throw error;
+    }
+}
+
+exports.getCharacterStatInfo = async function getCharacterStatInfo(ocid) {
+    if(!ocid || typeof ocid !== 'string') {
+        throw new Error('Invalid OCID');
+    }
+
+    try{
+        const response = await axios({
+            url : MAPLE_API_URL + '/maplestory/v1/character/stat',
+            method: 'GET',
+            headers : {
+                'x-nxopen-api-key' : MAPLE_API_KEY
+            },
+            params : {
+                ocid : encodeURIComponent(ocid)
+            }
+        });
+
+        console.log(response.data);
+
+        return response.data;
+    }catch (error) {
+        if(error.response) {
+            console.error('API Error Response : ' , error.response.data);
+        }else{
+            console.error('Request Error : ', error.message);
+        }
+        throw error;
+    }
+}
+
+exports.getCharacterHyperStatInfo = async function getCharacterHyperStatInfo(ocid) {
+    if(!ocid || typeof ocid !== 'string') {
+        throw new Error('Invalid OCID');
+    }
+    try{
+        const response = await axios({
+            url : MAPLE_API_URL + '/maplestory/v1/character/hyper-stat',
+            method : 'GET',
+            headers : {'x-nxopen-api-key' : MAPLE_API_KEY},
+            params : {
+                ocid : encodeURIComponent(ocid)
+            }
+        });
+
+        console.log(response.data);
+
+        return response.data;
+    }catch(error){
+        if(error.response) {
+            console.error('API Error Response : ', error.response.data);
+        }else{
+            console.error('Request Error : ', error.message);
+        }
+        throw error;
+    }
+}
+
+exports.getCharacterPropensityInfo = async function getCharacterPropensity(ocid) {
+
+    if(!ocid || typeof ocid !== 'string') {
+        throw new Error('Invalid OCID');
+    }
+
+    try{
+        const response = await axios({
+           url : MAPLE_API_URL + '/maplestory/v1/character/propensity',
+           method : 'GET',
+           headers : {
+               'x-nxopen-api-key' : MAPLE_API_KEY
+           },
+           params : {
+               ocid : ocid
+           }
+        });
+
+        console.log(response.data);
+
+        return response.data;
+    }catch (error) {
+        if(error.response) {
+            console.error('API Error Response : ' ,error.response.data);
+        }else{
+            console.error('Request Error : ' , error.message);
+        }
+
+        throw error;
+    }
+}
+
+exports.getCharacterAbilityInfo = async function getCharacterAbilityInfo(ocid) {
+    if(!ocid || typeof ocid !== 'string') {
+        throw new Error('Invalid OCID');
+    }
+
+    try{
+        const response = await axios({
+           url : MAPLE_API_URL + '/maplestory/v1/character/ability',
+           method : 'GET',
+           headers : {
+               'x-nxopen-api-key' : MAPLE_API_KEY
+           },
+           params : {
+               ocid : ocid
+           }
+        });
+
+        console.log(response.data);
+
+        return response.data;
+    }catch(error) {
+        if(error.response) {
+            console.error('API Error Response : ', error.response.data);
+        }else{
+            console.error('Request Error : ' , error.message);
+        }
+
+        throw error;
+    }
+}
+
+exports.getCharacterItemEquipmentInfo = async function getCharacterItemEquipmentInfo(ocid) {
+    if(!ocid || typeof ocid !== 'string') {
+        throw new Error('Invalid OCID');
+    }
+
+    try{
+        const response = await axios({
+           url : MAPLE_API_URL + '/maplestory/v1/character/item-equipment',
+           method : 'GET',
+           headers : {
+               'x-nxopen-api-key' : MAPLE_API_KEY
+           },
+           params : {
+               ocid : ocid
+           }
+        });
+
+        console.log(response.data);
+
+        return response.data;
+    }catch(error) {
+        if(error.response) {
+            console.error('API Error Response : ', error.response.data);
+        }else {
+            console.error('Request Error : ', error.message);
+        }
+
         throw error;
     }
 }
--- routes/character.js
+++ routes/character.js
@@ -7,21 +7,127 @@ require("dotenv").config();
 const API_KEY = process.env.MAPLE_API_KEY;
 const url = process.env.MAPLE_API_URL;
 
-router.get('/list', async function (req, res) {
-    const characterName = req.query.characterName;
+router.get('/basic-info', async function (req, res) {
+    const characterName = req.query.character_name;
     console.log(characterName);
     try{
         const encodedCharacterName = encodeURIComponent(characterName);
         const ocid = await maplestory.getCharacterOCID(encodedCharacterName);
-        console.log(ocid);
-        const date = util.getFormattedDateKST();
-        const characterData = await maplestory.getCharacterBasic(ocid, date);
+        const characterData = await maplestory.getCharacterBasicInfo(ocid);
 
         res.json(characterData);
     }catch(error){
         console.error(error);
         res.status(500).json({
             error : 'Failed to retrieve character data'
+        });
+    }
+});
+
+// 인기도
+router.get('/popularity-info', async function (req, res) {
+    const characterName = req.query.character_name;
+
+    try{
+        const encodedCharacterName = encodeURIComponent(characterName);
+        const ocid = await maplestory.getCharacterOCID(encodedCharacterName);
+        const characterPopularity = await maplestory.getCharacterPopularityInfo(ocid);
+
+        res.json(characterPopularity);
+    }catch(error){
+        console.error(error);
+        res.status(500).json({
+            error : 'Failed to retrieve character popularity data'
+        })
+    }
+});
+
+
+router.get('/stat-info', async function (req, res) {
+    const characterName = req.query.character_name;
+
+    try{
+        const encodedCharacterName = encodeURIComponent(characterName);
+        const ocid = await maplestory.getCharacterOCID(encodedCharacterName);
+        const characterStatInfo = await maplestory.getCharacterStatInfo(ocid);
+
+        res.json(characterStatInfo);
+    }catch(error) {
+        console.error(error);
+        res.status(500).json({
+            error : 'Failed to retrieve character stat data'
+        });
+    }
+});
+
+// hyper stat
+router.get('/hyper-stat-info', async function (req, res) {
+    const characterName = req.query.character_name;
+
+    try{
+        const encodedCharacterName = encodeURIComponent(characterName);
+        const ocid = await maplestory.getCharacterOCID(encodedCharacterName);
+        const characterHyperStatInfo = await maplestory.getCharacterHyperStatInfo(ocid);
+
+        res.json(characterHyperStatInfo);
+    }catch (error) {
+        console.error(error);
+        res.status(500).json({
+            error : 'Failed to retrieve character hyper-stat data'
+        });
+    }
+});
+
+// 성향 조회
+router.get('/propensity-info', async function(req,res,next){
+   const characterName = req.query.character_name;
+
+   try{
+       const encodedCharacterName = encodeURIComponent(characterName);
+       const ocid = await maplestory.getCharacterOCID(encodedCharacterName);
+       const characterPropensity = await maplestory.getCharacterPropensityInfo(ocid);
+
+       res.json(characterPropensity);
+   }catch(error) {
+       console.error(error);
+       res.status(500).json({
+           error : 'Failed to retrieve character propensity data'
+       });
+   }
+});
+
+// 어빌리티 조회
+router.get('/ability-info', async function(req,res,next){
+   const characterName = req.query.character_name;
+
+   try{
+       const encodedCharacterName = encodeURIComponent(characterName);
+       const ocid = await maplestory.getCharacterOCID(encodedCharacterName);
+       const characterAbilityInfo = await maplestory.getCharacterAbilityInfo(ocid);
+
+       res.json(characterAbilityInfo);
+   }catch(error){
+       console.error(error);
+       res.status(500).json({
+           error : 'Failed to retrieve character ability data'
+       });
+   }
+});
+
+// 장착 장비 조회 ( 캐시템 제외)
+router.get('/item-equipment-info', async function (req,res,next){
+    const characterName = req.query.character_name;
+
+    try{
+        const encodedCharacterName = encodeURIComponent(characterName);
+        const ocid = await maplestory.getCharacterOCID(encodedCharacterName);
+        const characterItemEquipmentInfo = await maplestory.getCharacterItemEquipmentInfo(ocid);
+
+        res.json(characterItemEquipmentInfo);
+    }catch(error) {
+        console.error(error);
+        res.status(500).json({
+            error : 'Failed to retrieve character item equipment data'
         });
     }
 });
