2f22fa99e5a091eca1b7250472b4ddf78b0fc818
yeonsang <wjddustkd45@gmail.com>
2024-11-13 21:18:34 +0900

    refactor : rank 수정

--- util/errorHandler.js
+++ util/errorHandler.js
@@ -1,6 +1,5 @@
 
 exports.handlerErrorResponse = function (response, res) {
-    console.log(JSON.stringify(response));
     if(!response.success) {
         return res.status(400).json({
             result : response.message || 'Unknown error occurred'
